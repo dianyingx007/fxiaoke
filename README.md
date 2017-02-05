d2e7107da6f3bf3b5e473553e5ecd4ddf576461e
danny <dianyingx007@163.com>
2017-02-05 16:08:09 +0800

    初始化

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # fxiaoke
+
 仿纷享销客
+
+[https://www.fxiaoke.com/](https://www.fxiaoke.com/)
+
+![截图](网页截图 - 纷享销客-min.png)
---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var reload = browserSync.reload;
+
+//静态服务器，并监听文件变化
+gulp.task('serve',['sass'],function(){
+    browserSync.init({
+        server: {
+            baseDir: "./",
+            index: "./index.html"
+        }
+    })
+
+    gulp.watch("css/*.scss",['sass']);
+    gulp.watch(["*.html","js/*.js"]).on('change',reload);
+});
+
+// scss编译后的css将注入到浏览器里实现更新
+gulp.task('sass',function(){
+    return gulp.src("css/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("css"))
+        .pipe(reload({stream:true}));
+});
+
+gulp.task('default',['serve']);
