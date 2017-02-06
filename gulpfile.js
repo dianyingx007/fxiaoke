b5815dae2cd894bc3c3637156cc499dd3f47eb01
danny <dianyingx007@163.com>
2017-02-06 17:58:51 +0800

    完成feature特性部分，并完成动画

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,20 @@ var sass = require('gulp-sass');
 var reload = browserSync.reload;
 
 //静态服务器，并监听文件变化
-gulp.task('serve',['sass'],function(){
+gulp.task('serve', ['sass'], function() {
     browserSync.init({
         server: {
             baseDir: "./",
             index: "./index.html"
         }
-    })
+    });
 
     gulp.watch("css/*.scss",['sass']);
     gulp.watch(["*.html","js/*.js"]).on('change',reload);
 });
 
 // scss编译后的css将注入到浏览器里实现更新
-gulp.task('sass',function(){
+gulp.task('sass', function(){
     return gulp.src("css/*.scss")
         .pipe(sass())
         .pipe(gulp.dest("css"))
--- js/fxiaoke.js
+++ js/fxiaoke.js
@@ -1,7 +1,22 @@
 (function($) {
 	var changeHeaderClass = function() {
-		if($(window).scrollTop()>80) $('header').removeClass('transparent');
+		var contop = $(window).scrollTop();
+		var height = $(window).height();
+		
+		//判断header
+		if(contop>80) $('header').removeClass('transparent');
 		else $('header').addClass('transparent');
+		
+		//判断feature动画
+		var feature_list = $('.feature_one');
+		feature_list.each(function() {
+			var item_top = $(this).offset().top - contop;
+			var item_bot = item_top + $(this).height();
+			if(item_top < height || item_bot <= height) {
+				$($(this).children()[0]).addClass('ani_left');
+				$($(this).children()[1]).addClass('ani_right');
+			}
+		});
 	};
 	$(window).on("scroll",changeHeaderClass);
 })(jQuery);
