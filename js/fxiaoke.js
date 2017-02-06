6cac9eb040254a4195569b86b068a825afd5a4f5
danny <dianyingx007@163.com>
2017-02-06 19:59:46 +0800

    1、对滚动事件进行优化，使用节流 2、对布局进行优化修改

--- js/fxiaoke.js
+++ js/fxiaoke.js
@@ -1,22 +1,56 @@
 (function($) {
+	//header显示判定
 	var changeHeaderClass = function() {
+		if($(window).scrollTop() > $('header').height()) $('header').removeClass('transparent');
+		else $('header').addClass('transparent');
+	};
+	//$(window).on("scroll", changeHeaderClass);
+	
+	//feature中的动画判定
+	var feature_show = function() {
 		var contop = $(window).scrollTop();
 		var height = $(window).height();
-		
-		//判断header
-		if(contop>80) $('header').removeClass('transparent');
-		else $('header').addClass('transparent');
-		
-		//判断feature动画
 		var feature_list = $('.feature_one');
 		feature_list.each(function() {
+			var children = $(this).children().children();
 			var item_top = $(this).offset().top - contop;
 			var item_bot = item_top + $(this).height();
-			if(item_top < height || item_bot <= height) {
-				$($(this).children()[0]).addClass('ani_left');
-				$($(this).children()[1]).addClass('ani_right');
+			if((item_top >= 0 && item_top < height) || (item_bot > 0 && item_bot <= height)) {
+				$(children[0]).addClass('ani_left');
+				$(children[1]).addClass('ani_right');
 			}
 		});
 	};
-	$(window).on("scroll",changeHeaderClass);
+	feature_show();
+	//$(window).on("scroll", feature_show);
+	
+	// 简单的节流函数
+	function throttle(func, wait, mustRun) {
+	    var timeout,
+	        startTime = new Date();
+	 
+	    return function() {
+	        var context = this,
+	            args = arguments,
+	            curTime = new Date();
+	 
+	        clearTimeout(timeout);
+	        // 如果达到了规定的触发时间间隔，触发 handler
+	        if(curTime - startTime >= mustRun){
+	            func.apply(context,args);
+	            startTime = curTime;
+	        // 没达到触发间隔，重新设定定时器
+	        }else{
+	            timeout = setTimeout(func, wait);
+	        }
+	    };
+	};
+	
+	var scrollEvent = throttle(function() {
+		//滚动中的真正的操作
+		changeHeaderClass();
+		feature_show();
+	},100,200)
+	$(window).on("scroll", scrollEvent);
+	
 })(jQuery);
